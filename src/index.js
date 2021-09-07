129891fff3ec96f3bfe7b8cd61d8735b819d9933
Possible <ahunanyabenjamin@gmail.com>
2021-09-08 00:33:13 +0100

    port addition

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
+const port = process.env.PORT || 3000
 const publicPath = path.join(__dirname, '../public')
 app.use(express.static(publicPath))
 // app.get('/resume', (req, res) => {
@@ -37,4 +38,4 @@ app.use(express.static(publicPath))
 //   }
 // })
 
-app.listen(3000, console.log('App is running on port 3000'))
+app.listen(port, console.log(`App is running on port ${port}`))
